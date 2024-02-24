95434165a152b61b5fa3a59d029826e898bd2675
yuxu060324 <yu.y.200017@gmail.com>
2024-02-25 03:19:57 +0900

    add global_file

---
+++ line_global.py
@@ -0,0 +1,10 @@
+# 環境変数などのすべてのファイルで使用できる変数の定義ファイル
+
+# アイコン設定
+icon_file_dic = {
+    "task": "task.jpg",
+    "event": "event.jpg",
+    "game": "game.jpg",
+    "commu": "commu.jpg",
+    "other": "other.jpg",
+}
