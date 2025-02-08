9531b54380ac11cbbc3bebc4f18874e96dfe63aa
yuxu060324 <yu.y.200017@gmail.com>
2025-02-09 01:59:24 +0900

    [main] update log (exist)

--- common_global.py
+++ common_global.py
@@ -15,19 +15,22 @@ log_file_name = "project"
 
 # logger
 def getMyLogger(name):
-    project_path = os.path.dirname(os.path.abspath(__file__))
-    log_file_path = os.path.join(project_path, "log", f'{log_file_name}.log')
+
+    print("Create Logger")
+
+    log_file_path = os.path.join(HOME_ABSPATH, "log", f'{log_file_name}.log')
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
     # ディレクトリの存在確認
-    if not os.path.isdir(os.path.join(project_path, "log")):
+    if not os.path.isdir(os.path.join(HOME_ABSPATH, "log")):
         # ディレクトリの作成
-        os.makedirs(os.path.join(project_path, "log"))
+        os.makedirs(os.path.join(HOME_ABSPATH, "log"))
         # ファイルの存在確認
         if not os.path.isfile(log_file_path):
             # ファイルの作成
             with open(log_file_path, "w"):
+                print(f'file created:{log_file_path}')
                 pass
 
     handler = logging.FileHandler(log_file_path, encoding="utf-8")
