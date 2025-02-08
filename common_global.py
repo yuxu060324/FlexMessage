5749b614f5d6cd1ea060bf15f27a00ea2429e590
yuxu060324 <yu.y.200017@gmail.com>
2025-02-08 17:32:14 +0900

    modified:   common_global.py

--- common_global.py
+++ common_global.py
@@ -19,6 +19,17 @@ def getMyLogger(name):
     log_file_path = os.path.join(project_path, "log", f'{log_file_name}.log')
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
+
+    # ディレクトリの存在確認
+    if not os.path.isdir(os.path.join(project_path, "log")):
+        # ディレクトリの作成
+        os.makedirs(os.path.join(project_path, "log"))
+        # ファイルの存在確認
+        if not os.path.isfile(log_file_path):
+            # ファイルの作成
+            with open(log_file_path, "w"):
+                pass
+
     handler = logging.FileHandler(log_file_path, encoding="utf-8")
     handler.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(levelname)-9s %(asctime)s [%(filename)s:%(lineno)d %(funcName)s] %(message)s')
