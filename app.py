987a09169aad346bf08c09feeeac85696bee5c8c
yuxu060324 <yu.y.200017@gmail.com>
2025-08-02 04:10:40 +0900

    change get_weather api

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 from flask import Flask, request, abort, send_from_directory
 from json_global import *
 from linebot import (
-    LineBotApi, WebhookHandler
+	LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+	InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, FlexSendMessage, TextSendMessage
+	MessageEvent, TextMessage, FlexSendMessage, TextSendMessage
 )
 from GoogleSchedule import get_calendar_event
 from JsonControl import (
-    package_message_one_day,
-    package_carousel_message,
-    package_message_error
+	package_message_one_day,
+	package_carousel_message,
+	package_message_error
 )
+from GetWeather import get_weather, get_weather_path
 
 set_environ(build_env="")
 line_bot_api = LineBotApi(os.getenv("LINE_BOT_CHANNEL_ACCESS_TOKEN"))
@@ -22,88 +23,106 @@ handler = WebhookHandler(os.getenv("LINE_BOT_CHANNEL_SECRET"))
 
 app = Flask(__name__)
 
+# default(sleep対策用)
 @app.route("/")
 def home():
-    return "HOME"
+	return "HOME"
+
+# 画像変更
+@app.route("/change_image")
+def change_image():
+
+	# 画像生成
+	get_weather(place_code="130000")
+
+	return
+
+# 天気画像表示
+@app.route("/view_weather_image")
+def view_weather_image():
+
+	get_weather_path()
+
+	return
 
 @app.route("/out/<filename>")
 def view_image(filename):
 
-    logger.debug(f'dirname: {OUT_FOLDER_PATH}')
-    logger.debug(f'filename: {filename}')
+	logger.debug(f'dirname: {OUT_FOLDER_PATH}')
+	logger.debug(f'filename: {filename}')
 
-    return send_from_directory(OUT_FOLDER_PATH, filename)
+	return send_from_directory(OUT_FOLDER_PATH, filename)
 
 # LINEのユーザからの情報を受け取る。
 @app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers['X-Line-Signature']
+	signature = request.headers['X-Line-Signature']
 
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+	body = request.get_data(as_text=True)
+	app.logger.info("Request body: " + body)
 
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+	try:
+		handler.handle(body, signature)
+	except InvalidSignatureError:
+		abort(400)
 
-    return 'OK'
+	return 'OK'
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
-    get_schedule_kind = schedule_kind.NUM_KIND
+	get_schedule_kind = schedule_kind.NUM_KIND
 
-    logger.info(f'Event.message: {event.message.text}')
+	logger.info(f'Event.message: {event.message.text}')
 
-    # 固定文以外のメッセージの場合は、受け取ったメッセージをそのまま返す
-    if not event.message.text in LINE_MESSAGE_ACTION_LIST:
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text)
-        )
-        return
+	# 固定文以外のメッセージの場合は、受け取ったメッセージをそのまま返す
+	if not event.message.text in LINE_MESSAGE_ACTION_LIST:
+		line_bot_api.reply_message(
+			event.reply_token,
+			TextSendMessage(text=event.message.text)
+		)
+		return
 
-    if event.message.text == LINE_MESSAGE_ACTION_LIST[0]:           # 今日の予定を通知
-        get_schedule_kind = schedule_kind.TODAY
-    elif event.message.text == LINE_MESSAGE_ACTION_LIST[1]:         # 明日の予定を通知
-        get_schedule_kind = schedule_kind.TOMORROW
-    elif event.message.text == LINE_MESSAGE_ACTION_LIST[2]:         # 1週間の予定を通知
-        get_schedule_kind = schedule_kind.WEEKLY
+	if event.message.text == LINE_MESSAGE_ACTION_LIST[0]:           # 今日の予定を通知
+		get_schedule_kind = schedule_kind.TODAY
+	elif event.message.text == LINE_MESSAGE_ACTION_LIST[1]:         # 明日の予定を通知
+		get_schedule_kind = schedule_kind.TOMORROW
+	elif event.message.text == LINE_MESSAGE_ACTION_LIST[2]:         # 1週間の予定を通知
+		get_schedule_kind = schedule_kind.WEEKLY
 
-    try:
-        # Googleカレンダーから予定の取得
-        events = get_calendar_event(schedule_kind=get_schedule_kind)
+	try:
+		# Googleカレンダーから予定の取得
+		events = get_calendar_event(schedule_kind=get_schedule_kind)
 
-        if events is None:
-            logger.warning("Events is Empty")
-            raise EnvironmentError("Could not retrieve event.")
+		if events is None:
+			logger.warning("Events is Empty")
+			raise EnvironmentError("Could not retrieve event.")
 
-        if get_schedule_kind == schedule_kind.WEEKLY:
-            payload = package_carousel_message(schedule_dict=events)
-        else:
-            payload = package_message_one_day(events_list=events)
+		if get_schedule_kind == schedule_kind.WEEKLY:
+			payload = package_carousel_message(schedule_dict=events)
+		else:
+			payload = package_message_one_day(events_list=events)
 
-        # ログファイルに出力
-        logger.debug(f'payload: {payload}')
+		# ログファイルに出力
+		logger.debug(f'payload: {payload}')
 
-    except Exception as e:
-        payload = package_message_error()
-        logger.warning(f'{e.__class__.__name__}: {e}')
+	except Exception as e:
+		payload = package_message_error()
+		logger.warning(f'{e.__class__.__name__}: {e}')
 
-    if payload is not None:
-        # FlexMessage形式のメッセージ作成
-        obj = FlexSendMessage(alt_text="Message", contents=payload)
-        # LINEメッセージの送信
-        line_bot_api.reply_message(
-            reply_token=event.reply_token,
-            messages=obj
-        )
-    else:
-        logger.info("This transaction is failed")
+	if payload is not None:
+		# FlexMessage形式のメッセージ作成
+		obj = FlexSendMessage(alt_text="Message", contents=payload)
+		# LINEメッセージの送信
+		line_bot_api.reply_message(
+			reply_token=event.reply_token,
+			messages=obj
+		)
+	else:
+		logger.info("This transaction is failed")
 
 
 if __name__ == "__main__":
-    logger.info("Flask App running")
-    app.run(threaded=True)
+	logger.info("Flask App running")
+	app.run(threaded=True)
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from JsonControl import (
 	_package_message_one_day_none_image,
 	package_carousel_message
 )
+from GetWeather import get_weather, get_weather_path
 
 os.environ["SET_BUILD"] = "LOCAL"
 
@@ -19,19 +20,100 @@ set_environ(build_env="")
 line_bot_api = LineBotApi(os.environ['LINE_BOT_CHANNEL_ACCESS_TOKEN'])
 
 
+def create_message_image_test_message():
+
+	payload = {
+		"type": "bubble",
+		"size": "mega",
+		"header": {
+			"type": "box",
+			"layout": "horizontal",
+			"contents": [
+				{
+					"type": "box",
+					"layout": "vertical",
+					"contents": [
+						{
+							"type": "text",
+							"text": "DATE",
+							"color": "#ffffffB0",
+							"size": "sm"
+						},
+						{
+							"type": "text",
+							"text": "08 / 01 ( 金 )",
+							"color": "#ffffff",
+							"size": "xl",
+							"flex": 4,
+							"weight": "bold"
+						}
+					]
+				}
+			],
+			"paddingAll": "20px",
+			"backgroundColor": "#0367D3",
+			"spacing": "md",
+			"height": "90px"
+		},
+		"hero": {
+			"type": "image",
+			"url": get_weather_path(),
+			"size": "full",
+			"aspectRatio": "16:9"
+		},
+		"body": {
+			"type": "box",
+			"layout": "vertical",
+			"contents": [
+				{
+					"type": "text",
+					"text": "予定なし",
+					"color": "#0000a0",
+					"size": "xl",
+					"weight": "bold"
+				}
+			],
+			"margin": "lg",
+			"paddingAll": "lg",
+			"alignItems": "center"
+		},
+		"footer": {
+			"type": "box",
+			"layout": "vertical",
+			"contents": [
+				{
+					"type": "text",
+					"text": "\"Google Calendar\" を開く",
+					"color": "#0000ff",
+					"action": {
+						"type": "uri",
+						"label": "action",
+						"uri": "https://www.google.com/"
+					},
+					"decoration": "underline"
+				}
+			]
+		}
+	}
+
+	return payload
+
 # main( for debug )
 def main():
-	get_schedule_kind = schedule_kind.WEEKLY
+	get_schedule_kind = schedule_kind.TODAY
 
-	# Googleカレンダーから予定の取得
-	events = get_calendar_event(schedule_kind=get_schedule_kind)
-	if events is None:
-		logger.debug("Events is empty")
-		return None
+	# # Googleカレンダーから予定の取得
+	# events = get_calendar_event(schedule_kind=get_schedule_kind)
+	# if events is None:
+	# 	logger.debug("Events is empty")
+	# 	return None
 
-	logger.info(events)
+	# logger.info(events)
 
-	# eventsのpackage(画像有の1日の予定)
+	# 画像表示デバッグ用のpackage
+	payload = create_message_image_test_message()
+
+	# # eventsのpackage(画像有の1日の予定)
 	# payload = package_message_one_day(events_list=events)  # 一日のmessage
 	# logger.debug(f'payload: {payload}')
 
@@ -42,13 +124,13 @@ def main():
 	# )
 	# logger.debug(f'payload: {payload}')
 
-	# eventsのpackage(1週間の予定)
-	payload = package_carousel_message(schedule_dict=events)
-	logger.debug(f'payload: {payload}')
-
-	debug_file_path = os.path.join(HOME_ABSPATH, "TemplateMessage", "debug_message.json")
-	with open(debug_file_path, mode="w", encoding="utf-8") as f:
-		json.dump(payload, f, ensure_ascii=False, indent=4)
+	# # eventsのpackage(1週間の予定)
+	# payload = package_carousel_message(schedule_dict=events)
+	# logger.debug(f'payload: {payload}')
+	#
+	# debug_file_path = os.path.join(HOME_ABSPATH, "TemplateMessage", "debug_message.json")
+	# with open(debug_file_path, mode="w", encoding="utf-8") as f:
+	# 	json.dump(payload, f, ensure_ascii=False, indent=4)
 
 	if payload is not None:
 		# FlexMessageを作成(まだlineは送らない)
